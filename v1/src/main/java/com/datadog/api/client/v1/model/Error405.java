095c3c5ec8cb73e55072c6f712a59566f89fc81a
ci.datadog-api-spec <packages@datadoghq.com>
2019-10-30 15:02:56 +0000

    Regenerate client from commit d5a5b40 of spec repo

--- v1/src/main/java/com/datadog/api/client/v1/model/Error405.java
+++ v1/src/main/java/com/datadog/api/client/v1/model/Error405.java
@@ -21,10 +21,14 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import java.util.ArrayList;
 import java.util.List;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 /**
  * Error405
  */
+@JsonPropertyOrder({
+  Error405.JSON_PROPERTY_ERRORS
+})
 
 public class Error405 {
   public static final String JSON_PROPERTY_ERRORS = "errors";
