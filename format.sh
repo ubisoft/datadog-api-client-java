8cc5d549cd051ae1b0a3922e7ea95e9b0c54d04a
NouemanKHAL <noueman.khal@gmail.com>
2021-04-27 12:09:11 +0200

    Revert "[tests] Improve Java code formatting on generation (#753)"
    This reverts commit f117f0fc4984ab7695c667621569ca628375cd01.

--- format.sh
+++ format.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
 
-set -e
-
-if [ $# -ne 0 ]; then
-    JAR_FILE="/app/google-java-format.jar"
-    if [[ -e $JAR_FILE ]]; then
-        find "$@" -name '*.java' | xargs java -jar /app/google-java-format.jar -i
-    else
-        docker build -t google-java-format -f Dockerfile.format .
-        docker run -v ${PWD}/src:/app/src google-java-format --replace --set-exit-if-changed $@
-    fi
-fi
+find ./src -name '*.java' | xargs java -jar /app/google-java-format.jar -i
